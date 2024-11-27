d9591cfedcf3dc762c6139c5674af687fadfc903
Oscar <email@email.com>
2024-11-27 23:32:11 +0100

    saving...

--- src/CountdownTimer.jsx
+++ src/CountdownTimer.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from "react";
 
-const CountdownTimer = () => {
+const CountdownTimer = ({fecha}) => {
   // Estado para la fecha objetivo
   const [targetDate, setTargetDate] = useState(null);
-  const [targetDate2, setTargetDate2] = useState(null);
 
   // Estado para la cuenta regresiva
   const [timeRemaining, setTimeRemaining] = useState({});
-  const [timeRemaining2, setTimeRemaining2] = useState({});
 
   // Manejar cambios en los campos de fecha y hora
   const handleDateChange = (e) => {
@@ -21,7 +19,6 @@ const CountdownTimer = () => {
 
     const now = new Date();
     const difference = targetDate - now;
-    const difference2 = targetDate2 - now;
 
     if (difference <= 0) {
       setTimeRemaining({
@@ -38,38 +35,22 @@ const CountdownTimer = () => {
 
       setTimeRemaining({ days, hours, minutes, seconds });
     }
-
-    if (difference2 <= 0) {
-      setTimeRemaining2({
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      });
-    } else {
-      const days = Math.floor(difference2 / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((difference2 / (1000 * 60 * 60)) % 24);
-      const minutes = Math.floor((difference2 / (1000 * 60)) % 60);
-      const seconds = Math.floor((difference2 / 1000) % 60);
-
-      setTimeRemaining2({ days, hours, minutes, seconds });
-    }
   };
 
   // Actualizar la cuenta regresiva cada segundo
   useEffect(() => {
     const interval = setInterval(() => {
-        setTargetDate(new Date("2025-02-13T18:46"));
-        setTargetDate2(new Date("2025-02-01T00:00"));
+        // setTargetDate(new Date("2025-02-13T18:46"));
+        // setTargetDate2(new Date("2025-02-01T00:00"));
+        setTargetDate(new Date(fecha));
       calculateTimeRemaining();
     }, 1000);
 
     return () => clearInterval(interval); // Limpiar intervalo al desmontar el componente
-  }, [targetDate,targetDate2]);
+  }, [targetDate,fecha]);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Cuenta Regresiva</h1>
+    <>
       {/* <label htmlFor="datetime">Selecciona la fecha y hora objetivo:</label>
       <input
         id="datetime"
@@ -79,18 +60,14 @@ const CountdownTimer = () => {
       /> */}
       {targetDate && (
         <div>
-          <h2>Tiempo Restante</h2>
+          
           <p>
             {timeRemaining.days} días, {timeRemaining.hours} horas,{" "}
             {timeRemaining.minutes} minutos, {timeRemaining.seconds} segundos
           </p>
-          <p>
-            {timeRemaining2.days} días, {timeRemaining2.hours} horas,{" "}
-            {timeRemaining2.minutes} minutos, {timeRemaining2.seconds} segundos
-          </p>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import CountdownTimer from './CountdownTimer.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <CountdownTimer/>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Cuenta Regresiva</h1>
+      <h2>Tiempo Restante</h2>
+    </div>
+    <CountdownTimer fecha="2025-02-13T18:40"/>
+    <CountdownTimer fecha="2025-01-31T23:59"/>
+    <CountdownTimer fecha="2024-12-13T18:40"/>
   </React.StrictMode>,
 )
